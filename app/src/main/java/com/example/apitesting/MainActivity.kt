046556eba262da98ae9ce22385754ea085f33fd3
Sparsh <sparsh.chadha46@gmail.com>
2023-07-15 02:00:52 +0530

    fcfs achieved

--- app/src/main/java/com/example/apitesting/MainActivity.kt
+++ app/src/main/java/com/example/apitesting/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.apitesting
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.selects.select
@@ -13,21 +15,26 @@ import kotlinx.coroutines.withContext
 class MainActivity : AppCompatActivity() {
     private val TAG = "MainActTag"
 
-//    Make multiple network calls and use the result in FCFS manner
+    //    Make multiple network calls and use the result in FCFS manner
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        lifecycleScope.launch {
-            val commentsDeferred = async { give7SecondsDelay() }
-            val albumsDeferred = async { give2SecDelay() }
 
-            val deferredList = listOf(commentsDeferred, albumsDeferred)
+//    1. order of await matters
+//    2. Select expression makes it possible to await multiple suspending functions simultaneously and select the first one that becomes available. https://kotlinlang.org/docs/select-expression.html
+
+//      Use of select -
+        lifecycleScope.launch {
+            val deferred1 = async { give7SecondsDelay() }
+            val deferred2 = async { give2SecDelay() }
+
+            val deferredList = listOf(deferred1, deferred2)
 
             val result = withContext(lifecycleScope.coroutineContext) {
                 select {
-                    commentsDeferred.onAwait { it }
-                    albumsDeferred.onAwait { it }
+                    deferred1.onAwait { it }
+                    deferred2.onAwait { it }
                 }
             }
 
@@ -39,15 +46,31 @@ class MainActivity : AppCompatActivity() {
                 Toast.makeText(applicationContext, otherResult, Toast.LENGTH_SHORT).show()
             }
         }
+
+
+//        All 4 results will be logged together after 7 seconds have elapsed.
+        lifecycleScope.launch {
+            val deferred1 = async { give7SecondsDelay() }
+            val deferred2 = async { give2SecDelay() }
+            val deferred3 = async { give7SecondsDelay() }
+            val deferred4 = async { give2SecDelay() }
+
+            val results = awaitAll(deferred1, deferred2, deferred3, deferred4)
+
+            for (result in results) {
+                Log.d(TAG, "awaitAll : $result")
+            }
+        }
+
     }
 
 
-    private suspend fun give7SecondsDelay() : String {
+    private suspend fun give7SecondsDelay(): String {
         delay(7000L)
         return "7 Second delay"
     }
 
-    private suspend fun give2SecDelay() : String {
+    private suspend fun give2SecDelay(): String {
         delay(2000L)
         return "2 Second delay"
     }
